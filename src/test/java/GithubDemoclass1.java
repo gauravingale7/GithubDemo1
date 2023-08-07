8b88c61acf9b45234e696c1e874c6c11e7966d7d
Lenovo <Lenovo@Gaurav>
2023-08-07 23:50:34 +0530

    First Github commit

---
+++ src/test/java/GithubDemoclass1.java
@@ -0,0 +1,22 @@
+
+public class GithubDemoclass1 {
+	
+	public void sum()
+	{
+		int a=10,b=40;
+		int c=a+b;
+		System.out.println("sum= "+c);
+	}
+
+	public static void main(String[] args) {
+	
+		GithubDemoclass1 gh=new GithubDemoclass1();
+		
+		gh.sum();
+				
+
+	}
+
+	
+
+}
