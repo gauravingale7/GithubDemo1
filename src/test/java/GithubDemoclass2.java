5c89e7862749213823102f3cf621ad77fa313755
Lenovo <Lenovo@Gaurav>
2023-08-08 11:35:52 +0530

    Div code added

---
+++ src/test/java/GithubDemoclass2.java
@@ -0,0 +1,12 @@
+
+public class GithubDemoclass2 {
+	public static void main(String[] args) {
+	
+			int a=10,b=40;
+			int c=a+b;
+			System.out.println("sum= "+c);
+			
+			int d=a/b;
+			System.out.println("Div: "+d);
+		}
+}
