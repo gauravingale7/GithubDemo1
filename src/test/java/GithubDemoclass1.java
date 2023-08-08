77171a51319ecd3247dc81829a58dd380165196d
Lenovo <Lenovo@Gaurav>
2023-08-08 11:25:16 +0530

    BranchX added

--- src/test/java/GithubDemoclass1.java
+++ src/test/java/GithubDemoclass1.java
@@ -1,22 +1,9 @@
 
 public class GithubDemoclass1 {
-	
-	public void sum()
-	{
-		int a=10,b=40;
-		int c=a+b;
-		System.out.println("sum= "+c);
-	}
-
 	public static void main(String[] args) {
 	
-		GithubDemoclass1 gh=new GithubDemoclass1();
-		
-		gh.sum();
-				
-
-	}
-
-	
-
+			int a=10,b=40;
+			int c=a+b;
+			System.out.println("sum= "+c);
+		}
 }
